857ac7898d37ec92957013eec0b1639e98554be6
赖成杰 <lai.cj@hengkangit.com>
2026-04-15 13:53:41 +0800

    Stop tracking bin folder and add to .gitignore

--- MyWebAPI.WebAPI/obj/Debug/net8.0/AvaloniaWebAPI.WebAPI.AssemblyInfo.cs
+++ MyWebAPI.WebAPI/obj/Debug/net8.0/AvaloniaWebAPI.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaWebAPI.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f6276fbe031b67680fab1c5c2449d91a81c83d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaWebAPI.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaWebAPI.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
