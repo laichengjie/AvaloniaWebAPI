a768c619f12302a882bcea773e390eb567ba4e93
赖成杰 <lai.cj@hengkangit.com>
2026-09-23 15:22:58 +0800

    1

--- AvaloniaWebAPI.Core/Entities/mc_general_act.cs
+++ AvaloniaWebAPI.Core/Entities/mc_general_act.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace AvaloniaWebAPI.Core.Entities
 {
     /// <summary>
-    /// 通用活动
+    /// 通用活动（补全字段版）
     /// </summary>
     [Table("mc_general_act")]
     public class mc_general_act
@@ -15,6 +15,12 @@ namespace AvaloniaWebAPI.Core.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int id { get; set; }
+
+        /// <summary>
+        /// 删除标记（0未删除，1已删除）
+        /// </summary>
+        [Column("is_deleted")]
+        public int is_deleted { get; set; }
 
         /// <summary>
         /// 集团ID
@@ -113,7 +119,7 @@ namespace AvaloniaWebAPI.Core.Entities
         public int benchmark_price_id { get; set; }
 
         /// <summary>
-        /// 活动参与次数(每一个人参与最大次数)
+        /// 活动参与次数
         /// </summary>
         [Column("per_max_times")]
         public int per_max_times { get; set; }
@@ -137,16 +143,22 @@ namespace AvaloniaWebAPI.Core.Entities
         public string remark { get; set; } = string.Empty;
 
         /// <summary>
-        /// 参与商品-促销商品设置方式（字典：MC_ApplyProductModel）
+        /// 参与商品-促销商品设置方式
         /// </summary>
         [Column("apply_product_modelid")]
         public int apply_product_modelid { get; set; }
 
         /// <summary>
-        /// 店铺范围(是否所有店铺):0 指定 1不限制 2排除
+        /// 店铺范围（0指定 1不限制 2排除）
         /// </summary>
         [Column("apply_all_shop")]
         public int apply_all_shop { get; set; } = 1;
+
+        /// <summary>
+        /// 状态
+        /// </summary>
+        [Column("status")]
+        public int status { get; set; }
 
         /// <summary>
         /// 审核人
@@ -161,34 +173,40 @@ namespace AvaloniaWebAPI.Core.Entities
         public DateTime check_time { get; set; } = DateTime.Now;
 
         /// <summary>
-        /// 修改人
+        /// 开单人id
         /// </summary>
-        [Column("modified_by")]
-        public string modified_by { get; set; } = "system";
+        [Column("creater_id")]
+        public int creater_id { get; set; }
 
         /// <summary>
-        /// 修改时间
-        /// </summary>
-        [Column("modified_time")]
-        public DateTime modified_time { get; set; } = DateTime.Now;
-
-        /// <summary>
-        /// 状态
-        /// </summary>
-        [Column("status")]
-        public int status { get; set; }
-
-        /// <summary>
-        /// 删除标记（0未删除，1已删除）
-        /// </summary>
-        [Column("is_deleted")]
-        public int is_deleted { get; set; }
-
-        /// <summary>
-        /// 适用场景(字典：MC_activity_scene)
+        /// 适用场景
         /// </summary>
         [Column("activity_scene")]
         public int activity_scene { get; set; }
+
+        /// <summary>
+        /// 是否开启优惠分摊
+        /// </summary>
+        [Column("preferential_divide_enabled")]
+        public int preferential_divide_enabled { get; set; }
+
+        /// <summary>
+        /// 第三方推送状态
+        /// </summary>
+        [Column("push_third_flag")]
+        public int push_third_flag { get; set; }
+
+        /// <summary>
+        /// 第三方推送时间
+        /// </summary>
+        [Column("push_third_time")]
+        public DateTime push_third_time { get; set; } = new DateTime(3000, 12, 31);
+
+        /// <summary>
+        /// 推送次数
+        /// </summary>
+        [Column("push_times")]
+        public int push_times { get; set; }
 
         /// <summary>
         /// 约束换购商品零售金额小于等于参与商品零售金额
@@ -206,7 +224,31 @@ namespace AvaloniaWebAPI.Core.Entities
         /// 商场业绩折扣
         /// </summary>
         [Column("mall_performance_discount")]
-        public decimal mall_performance_discount { get; set; }
+        public decimal mall_performance_discount { get; set; } = 1.000000m;
+
+        /// <summary>
+        /// 未添加换购商品弹窗提示
+        /// </summary>
+        [Column("popup_enabled")]
+        public int popup_enabled { get; set; }
+
+        /// <summary>
+        /// 删除人ID
+        /// </summary>
+        [Column("deleted_id")]
+        public int deleted_id { get; set; }
+
+        /// <summary>
+        /// 修改人ID
+        /// </summary>
+        [Column("modified_id")]
+        public int modified_id { get; set; }
+
+        /// <summary>
+        /// 是否引用
+        /// </summary>
+        [Column("is_reference")]
+        public int is_reference { get; set; }
 
         /// <summary>
         /// 是否已使用通知功能
